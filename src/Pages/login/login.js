2ecbec3844c4bccf1d0d21f464f9c46838ba2f3f
WrenayR <wrenj25@hotmail.co.uk>
2025-01-30 20:32:41 +0000

    Removed some unwanted things

---
+++ src/Pages/login/login.js
@@ -0,0 +1,12 @@
+import React, {Component} from "react";
+import './login.css';
+
+class Login extends Component {
+    render() {
+        return (
+            <div>test</div>
+        )
+    }
+}
+
+export default Login
